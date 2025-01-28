6815fead83b0e42215d849a7a623fb0f0f32190b
cdricn <cdricncdricn@gmail.com>
2025-01-28 21:26:27 +0800

    Added functions for remove, list and add

---
+++ InventoryApplication/InventoryManager.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using static ProductManagement.ProductManager;
+
+namespace ProductManagement
+{
+    internal class ProductManager
+    {
+        private readonly string _connectionString;
+
+        public ProductManager(String connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public class Product
+        {
+            public Guid ProductID { get; set; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            public double Price { get; set; }
+        }
+
+        public void AddProduct()
+        {
+            Console.WriteLine("\nInput the name of the product: ");
+            string productName = Console.ReadLine() ?? String.Empty;
+            int quantity;
+            double price;
+
+            // Check if quantity is in proper format
+            Console.WriteLine("\nInput the quantity of the product: ");
+            quantity = int.Parse(Console.ReadLine() ?? String.Empty);
+
+            // Check if price is in proper format
+            Console.WriteLine("\nInput the price of the product: ");
+            price = double.Parse(Console.ReadLine() ?? String.Empty);
+
+            // Execute query
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string query = $"INSERT INTO Products(ProductID, ProductName, QuantityInStock, Price) VALUES (newid(), @ProductName, @QuantityInStock, @Price)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ProductName", productName);
+                        command.Parameters.AddWithValue("@QuantityInStock", quantity);
+                        command.Parameters.AddWithValue("@Price", price);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        Console.WriteLine("\nProduct added successfully!");
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid product ID format. Please enter a valid GUID." + "\n " + e);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Action failed when adding product from table" + "\n " + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An unexpected error occurred." + "\n " + e);
+            }
+        }
+        
+        public void RemoveProduct()
+        {
+            Console.WriteLine("\nType the ID of the product you want to remove:");
+
+            // Execute delete query
+            try
+            {
+                Guid ProductID = Guid.Parse(Console.ReadLine() ?? String.Empty);
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string query = "DELETE FROM Products WHERE ProductID = @ProductID";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@ProductID", ProductID);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        // Check if query removed an entry
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("\nProduct removed successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nProduct ID does not exist.");
+                        }
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid product ID format. Please enter a valid GUID." + "\n " + e);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Action failed when deleting product from table" + "\n " + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An unexpected error occurred." + "\n " + e);
+            }
+        }
+        
+        public void UpdateProduct()
+        {
+            Console.WriteLine("\nEnter ProductID: ");
+            Guid id = Guid.Parse(Console.ReadLine() ?? String.Empty);
+
+            Console.WriteLine("\nEnter new item quantity: ");
+            int quantity = Int32.Parse(Console.ReadLine() ?? String.Empty);
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = $"UPDATE Products SET QuantityInStock = @Quantity WHERE ProductID = {id}";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Quantity", quantity);
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+        public void ListProducts()
+        {
+            List<Product> items = new List<Product>();
+
+            // Execute query
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT ProductID, ProductName, QuantityInStock, Price FROM Products";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Loop through and put each entry in List items
+                        while (reader.Read())
+                        {
+                            items.Add(new Product
+                            {
+                                ProductID = (Guid)reader["ProductID"],
+                                ProductName = (string)reader["ProductName"],
+                                Quantity = (int)reader["QuantityInStock"],
+                                Price = (double)reader["Price"],
+                            });
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+
+            // Display List items
+            foreach (Product item in items)
+            {
+                Console.WriteLine($"ID: {item.ProductID} | Name: {item.ProductName} |" +
+                    $" Quantity: {item.Quantity} | Price: {item.Price}");
+            }
+
+        }
+
+        public void GetTotalValue()
+        {
+
+        }
+    }
+}
--- InventoryApplication/Program.cs
+++ InventoryApplication/Program.cs
@@ -1,2 +1,125 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using ProductManagement;
+using static ProductManagement.ProductManager;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Get Connection String from the connected database
+        // If getting "keyword not supported" error, remove 'Trust Server Certificate=True' from connection string
+        string connectionString = "Data Source=Adrian\\MSSQLSERVER01;Initial Catalog=InventoryCRUD;Integrated Security=True;Trust Server Certificate=True";
+        ProductManager productManager = new ProductManager(connectionString);
+
+        Choices(productManager);
+    }
+
+    private static void Choices(ProductManager productManager)
+    {
+
+        // UI Start
+        Console.WriteLine("\nWelcome to Inventory Management App");
+        Console.WriteLine("\nUse the Up and Down key to navigate and press the Enter key to select an option:");
+
+        ConsoleKeyInfo key;
+        (int left, int top) = Console.GetCursorPosition();
+        bool isSelected = false;
+        int selector = 1;
+        string arrow = "->";
+
+        // Options UI
+        while (!isSelected)
+        {
+            // Prevent choices from being displayed again
+            Console.SetCursorPosition(left, top);
+
+            Console.WriteLine($" {(selector == 1 ? arrow : "  ")} View all products");
+            Console.WriteLine($" {(selector == 2 ? arrow : "  ")} Get inventory price");
+            Console.WriteLine($" {(selector == 3 ? arrow : "  ")} Add a product");
+            Console.WriteLine($" {(selector == 4 ? arrow : "  ")} Update a product");
+            Console.WriteLine($" {(selector == 5 ? arrow : "  ")} Remove a product");
+
+            key = Console.ReadKey(true);
+
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    selector--;
+                    break;
+                case ConsoleKey.DownArrow:
+                    selector++;
+                    break;
+                case ConsoleKey.Enter:
+                    isSelected = true;
+                    break;
+
+            }
+
+            // Make the selector loop through the options
+            if (selector > 5)
+            {
+                selector = 1;
+            }
+            else if (selector < 1)
+            {
+                selector = 5;
+            }
+        }
+
+        // Use class
+        switch (selector)
+        {
+            case 1:
+                Console.WriteLine("\nListing all Products...");
+                productManager.ListProducts();
+                break;
+            case 2:
+                Console.WriteLine("\nTotal Price:");
+                break;
+            case 3:
+                Console.WriteLine("\nAdd a new product:");
+                productManager.AddProduct();
+                break;
+            case 4:
+                Console.WriteLine("\nUpdate product quantity:");
+                productManager.UpdateProduct();
+                break;
+            case 5:
+                Console.WriteLine("\nRemove a product:");
+                Console.WriteLine("\nCurrent Product list: ");
+                productManager.ListProducts();
+
+                productManager.RemoveProduct();
+
+                break;
+        }
+    
+        // Ask user if they want to exit program
+        Console.WriteLine("\nDo you want to exit?: y/n");
+        string cont = Console.ReadLine() ?? String.Empty;
+        switch (cont)
+        {
+            case "y":
+                break;
+            case "n":
+                Console.Clear();
+                Choices(productManager);
+                break;
+            default:
+                Console.WriteLine("Exiting program");
+                break;
+        }
+    }
+
+    // Open connection with database
+    private static void OpenSqlConnection(string connectionString)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            Console.WriteLine("Successfully connected to the server!");
+        }
+    }
+}
