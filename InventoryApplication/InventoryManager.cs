c35b7f8df21519587f1ba07b560fba5615938e6a
cdricn <cdricncdricn@gmail.com>
2025-01-29 14:24:45 +0800

    Added update, add, and totalvalue

--- InventoryApplication/InventoryManager.cs
+++ InventoryApplication/InventoryManager.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
-using static ProductManagement.ProductManager;
 
 namespace ProductManagement
 {
@@ -17,58 +17,84 @@ namespace ProductManagement
             _connectionString = connectionString;
         }
 
-        public class Product
+        // Checks if quantity is a positive integer and returns value
+        private int CheckQuantity()
         {
-            public Guid ProductID { get; set; }
-            public string ProductName { get; set; }
-            public int Quantity { get; set; }
-            public double Price { get; set; }
+            int quantity;
+            Console.WriteLine("\nEnter the quantity of the product: ");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out quantity) && quantity > 0)
+                {
+                    break;
+                }
+                else { Console.WriteLine("\nEnter a valid integer. Please try again:"); }
+            }
+            return quantity;
         }
 
         public void AddProduct()
         {
-            Console.WriteLine("\nInput the name of the product: ");
-            string productName = Console.ReadLine() ?? String.Empty;
+            // Declare variables
+            string productName;
             int quantity;
             double price;
 
-            // Check if quantity is in proper format
-            Console.WriteLine("\nInput the quantity of the product: ");
-            quantity = int.Parse(Console.ReadLine() ?? String.Empty);
+            // Get and checks if the name is not a null or empty string
+            Console.WriteLine("\nEnter the name of the product: ");
+            while (true)
+            {
+                productName = (Console.ReadLine() ?? String.Empty).Trim();
+                if (string.IsNullOrEmpty(productName))
+                {
+                    Console.WriteLine("\nPlease Enter a name for product: ");
+                }
+                else { break; }
+            }
 
-            // Check if price is in proper format
-            Console.WriteLine("\nInput the price of the product: ");
-            price = double.Parse(Console.ReadLine() ?? String.Empty);
+            // Calls CheckQuantity function and stores in quantity
+            quantity = CheckQuantity();
 
-            // Execute query
+            // Get and checks if price is a valid positive number
+            Console.WriteLine("\nEnter the price of the product: ");
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out price) && price > 0)
+                {
+                    break;
+                }
+                else { Console.WriteLine("\nInput valid number. Please try again:"); }
+            }
+
+            // Placed code in try catch in case of error
             try
             {
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
                     connection.Open();
 
+                    // Insert entry into Products table
                     string query = $"INSERT INTO Products(ProductID, ProductName, QuantityInStock, Price) VALUES (newid(), @ProductName, @QuantityInStock, @Price)";
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        command.Parameters.AddWithValue("@ProductName", productName);
-                        command.Parameters.AddWithValue("@QuantityInStock", quantity);
-                        command.Parameters.AddWithValue("@Price", price);
+                    SqlCommand command = new SqlCommand(query, connection);
 
-                        int rowsAffected = command.ExecuteNonQuery();
+                    command.Parameters.Add("@ProductName", SqlDbType.NVarChar);
+                    command.Parameters["@ProductName"].Value = productName;
 
-                        Console.WriteLine("\nProduct added successfully!");
-                    }
+                    command.Parameters.Add("@QuantityInStock", SqlDbType.Int);
+                    command.Parameters["@QuantityInStock"].Value = quantity;
 
+                    command.Parameters.Add("@Price", SqlDbType.Float);
+                    command.Parameters["@Price"].Value = price;
+
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("\nProduct added successfully!");
                     connection.Close();
                 }
             }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Invalid product ID format. Please enter a valid GUID." + "\n " + e);
-            }
             catch (SqlException e)
             {
-                Console.WriteLine("Action failed when adding product from table" + "\n " + e);
+                Console.WriteLine("Action failed when inserting a product to table" + "\n " + e);
             }
             catch (Exception e)
             {
@@ -78,9 +104,9 @@ namespace ProductManagement
         
         public void RemoveProduct()
         {
-            Console.WriteLine("\nType the ID of the product you want to remove:");
+            Console.WriteLine("\nEnter the ID of the product you want to remove:");
 
-            // Execute delete query
+            // Placed code in try catch in case of error
             try
             {
                 Guid ProductID = Guid.Parse(Console.ReadLine() ?? String.Empty);
@@ -89,22 +115,24 @@ namespace ProductManagement
                 {
                     connection.Open();
 
+                    // Delete the entry from Products table that matches the input GUID
                     string query = "DELETE FROM Products WHERE ProductID = @ProductID";
-                    using (SqlCommand command = new SqlCommand(query, connection))
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Parameterize query
+                    command.Parameters.Add("@ProductID", SqlDbType.UniqueIdentifier);
+                    command.Parameters["@ProductID"].Value = ProductID;
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    // Check if query removed an entry
+                    if (rowsAffected > 0)
                     {
-                        command.Parameters.AddWithValue("@ProductID", ProductID);
-
-                        int rowsAffected = command.ExecuteNonQuery();
-
-                        // Check if query removed an entry
-                        if (rowsAffected > 0)
-                        {
-                            Console.WriteLine("\nProduct removed successfully!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("\nProduct ID does not exist.");
-                        }
+                        Console.WriteLine("\nProduct removed successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nProduct ID does not exist.");
                     }
 
                     connection.Close();
@@ -126,51 +154,66 @@ namespace ProductManagement
         
         public void UpdateProduct()
         {
-            Console.WriteLine("\nEnter ProductID: ");
-            Guid id = Guid.Parse(Console.ReadLine() ?? String.Empty);
+            Guid id;
+            int quantity;
 
-            Console.WriteLine("\nEnter new item quantity: ");
-            int quantity = Int32.Parse(Console.ReadLine() ?? String.Empty);
-
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            // Placed code in try catch in case of error
+            try
             {
-                connection.Open();
+                Console.WriteLine("\nEnter ProductID: ");
+                id = Guid.Parse(Console.ReadLine() ?? String.Empty);
 
-                string query = $"UPDATE Products SET QuantityInStock = @Quantity WHERE ProductID = {id}";
-                using (SqlCommand command = new SqlCommand(query, connection))
+                // Calls CheckQuantity function and stores in quantity
+                quantity = CheckQuantity();
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    command.Parameters.AddWithValue("@Quantity", quantity);
-                    command.ExecuteNonQuery();
-                }
+                    connection.Open();
 
-                connection.Close();
+                    string query = $"UPDATE Products SET QuantityInStock = @QuantityInStock WHERE ProductID = '{id}'";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    command.Parameters.Add("@QuantityInStock", SqlDbType.Int);
+                    command.Parameters["@QuantityInStock"].Value = quantity;
+
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("\nProduct updated successfully!");
+
+                    connection.Close();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid product ID format. Please enter a valid GUID." + "\n " + e);
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Action failed when updating product from table" + "\n " + e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An unexpected error occurred." + "\n " + e);
             }
         }
 
         public void ListProducts()
         {
-            List<Product> items = new List<Product>();
-
-            // Execute query
             using (SqlConnection connection = new SqlConnection(_connectionString))
             {
                 connection.Open();
 
-                string query = "SELECT ProductID, ProductName, QuantityInStock, Price FROM Products";
+                // Select all entries from Products table
+                string query = "SELECT * FROM Products";
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        // Loop through and put each entry in List items
+                        // Loop through the entries and display them in console
                         while (reader.Read())
                         {
-                            items.Add(new Product
-                            {
-                                ProductID = (Guid)reader["ProductID"],
-                                ProductName = (string)reader["ProductName"],
-                                Quantity = (int)reader["QuantityInStock"],
-                                Price = (double)reader["Price"],
-                            });
+                            Console.WriteLine($"ID: {reader["ProductID"]} | Name: {reader["ProductName"]} |" +
+                                $" Quantity: {reader["QuantityInStock"]} | Price: {reader["Price"]}");
                         }
                     }
                 }
@@ -178,18 +221,27 @@ namespace ProductManagement
                 connection.Close();
             }
 
-            // Display List items
-            foreach (Product item in items)
-            {
-                Console.WriteLine($"ID: {item.ProductID} | Name: {item.ProductName} |" +
-                    $" Quantity: {item.Quantity} | Price: {item.Price}");
-            }
-
         }
 
         public void GetTotalValue()
         {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
 
+                // Select the quantity and price columns from the Products table and sum them
+                string query = "SELECT SUM(QuantityInStock * Price) AS TotalPrice FROM Products";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"\nTotal price of inventory: {reader["TotalPrice"]}");
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- InventoryApplication/Program.cs
+++ InventoryApplication/Program.cs
@@ -8,21 +8,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Get Connection String from the connected database
-        // If getting "keyword not supported" error, remove 'Trust Server Certificate=True' from connection string
-        string connectionString = "Data Source=Adrian\\MSSQLSERVER01;Initial Catalog=InventoryCRUD;Integrated Security=True;Trust Server Certificate=True";
-        ProductManager productManager = new ProductManager(connectionString);
+        // Placed inside try catch in case error occurs
+        try
+        {
+            // Get Connection String from the connected database
+            // If getting "keyword not supported" error, remove 'Trust Server Certificate=True' from connection string
+            string connectionString = "Data Source=Adrian\\MSSQLSERVER01;Initial Catalog=InventoryCRUD;Integrated Security=True;Trust Server Certificate=True";
+            ProductManager productManager = new ProductManager(connectionString);
 
-        Choices(productManager);
+            // Call choices UI
+            Choices(productManager);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 
     private static void Choices(ProductManager productManager)
     {
-
-        // UI Start
         Console.WriteLine("\nWelcome to Inventory Management App");
         Console.WriteLine("\nUse the Up and Down key to navigate and press the Enter key to select an option:");
 
+        // Declare needed variables
         ConsoleKeyInfo key;
         (int left, int top) = Console.GetCursorPosition();
         bool isSelected = false;
@@ -32,17 +40,23 @@ class Program
         // Options UI
         while (!isSelected)
         {
-            // Prevent choices from being displayed again
+            // Gets console cursor position to render UI in the same spot
             Console.SetCursorPosition(left, top);
 
+            // Checks if selector is equal to the number of the option.
+            // If yes, it will display the arrow next to the option in the console
             Console.WriteLine($" {(selector == 1 ? arrow : "  ")} View all products");
             Console.WriteLine($" {(selector == 2 ? arrow : "  ")} Get inventory price");
             Console.WriteLine($" {(selector == 3 ? arrow : "  ")} Add a product");
             Console.WriteLine($" {(selector == 4 ? arrow : "  ")} Update a product");
             Console.WriteLine($" {(selector == 5 ? arrow : "  ")} Remove a product");
 
+            // Checks if user pressed a key
             key = Console.ReadKey(true);
 
+            // Navigate options with selector
+            // If user presses Up, reduce selector value and increase if user presses Down
+            // If user presses Enter, break the loop and proceed to class selection
             switch (key.Key)
             {
                 case ConsoleKey.UpArrow:
@@ -57,7 +71,8 @@ class Program
 
             }
 
-            // Make the selector loop through the options
+            // Make the selector loop through the options by settting it to the minimum
+            // or maximum value if its value goes under the minimum or over the maximum
             if (selector > 5)
             {
                 selector = 1;
@@ -68,7 +83,7 @@ class Program
             }
         }
 
-        // Use class
+        // Use the class methods depending on selector value
         switch (selector)
         {
             case 1:
@@ -76,7 +91,7 @@ class Program
                 productManager.ListProducts();
                 break;
             case 2:
-                Console.WriteLine("\nTotal Price:");
+                productManager.GetTotalValue();
                 break;
             case 3:
                 Console.WriteLine("\nAdd a new product:");
@@ -84,6 +99,9 @@ class Program
                 break;
             case 4:
                 Console.WriteLine("\nUpdate product quantity:");
+                Console.WriteLine("\nCurrent Product list: ");
+                productManager.ListProducts();
+
                 productManager.UpdateProduct();
                 break;
             case 5:
@@ -110,16 +128,6 @@ class Program
             default:
                 Console.WriteLine("Exiting program");
                 break;
-        }
-    }
-
-    // Open connection with database
-    private static void OpenSqlConnection(string connectionString)
-    {
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            connection.Open();
-            Console.WriteLine("Successfully connected to the server!");
         }
     }
 }
